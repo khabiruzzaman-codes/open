d83219d53ef0a9e758a184060e44fc8f5c354bcc
Khabiruzzaman <khabiruzzaman.codes@gmail.com>
2026-03-04 01:07:12 +0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,63 @@
+
+theClock();
+
+const search = document.querySelector("#search");
+const searchButton = document.querySelector("#searchButton");
+
+search.addEventListener("input", searching);
+
+search.addEventListener("keydown", (keyButton) => {
+  if (keyButton.key === "Enter") {
+    searching();
+  }
+});
+
+searchButton.addEventListener("click", searching);
+
+function searching() {
+  const cardGrid = document.querySelectorAll(".cardGrid");
+  const card = document.querySelectorAll(".cardGrid a");
+
+  cardGrid.forEach((grid) => {
+    grid.style.display = "flex";
+  })
+
+  const searchedText = search.value.trim().toLowerCase();
+
+  card.forEach((a) => {
+    const aName = a.getAttribute("data-name").toLowerCase();
+
+    if (aName.includes(searchedText) || aName === searchedText) {
+      a.style.display = "flex";
+    }
+    else {
+      a.style.display = "none";
+    }
+  });
+
+  cardGrid.forEach((grid) => {
+
+    if ([...grid.querySelectorAll("a")].every((a) => {
+
+      return a.style.display === 'none';
+    })) {
+      grid.style.display = 'none';
+    } else {
+      grid.style.display = 'flex'
+    }
+
+  });
+}
+
+
+function theClock() {
+  const clockDigit = document.querySelector("#clockDigit");
+  const clock = document.querySelector("#clock");
+  const exacttime = new Date();
+
+  const hours = exacttime.getHours();
+  const minutes = exacttime.getMinutes();
+
+  clockDigit.textContent = `${hours}.${minutes}`
+
+}
